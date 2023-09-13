9559d6fdc88caa96f4cb73eea2d4659053edbc72
Jin Young Bang <jybang@bu.edu>
2023-09-13 08:13:41 -0400

    refactor: use decorate to add env suffix to table_name

--- chalicelib/db.py
+++ chalicelib/db.py
@@ -9,50 +9,27 @@ class DBResource:
         self.resource = boto3.resource("dynamodb")
         self.primary_keys = {"zap-listings": "listingId"}
 
-    # def create_table(self, table_name: str, primary_key: str):
-    #     """Creates a table with a given primary_key"""
-    #     table = self.resource.create_table(
-    #         TableName=table_name,
-    #         KeySchema=[
-    #             {
-    #                 "AttributeName": primary_key,
-    #                 "KeyType": "HASH",  # Assuming you have a hash key
-    #             }
-    #         ],
-    #         AttributeDefinitions=[
-    #             {
-    #                 "AttributeName": primary_key,
-    #                 "AttributeType": "S",  # Assuming it's a string
-    #             }
-    #         ],
-    #         ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
-    #     )
-    #     # Wait until the table exists
-    #     table.meta.client.get_waiter("table_exists").wait(TableName=table_name)
-    #     print(table)
-    #     return table
+    def add_env_suffix(func):
+        def wrapper(self, table_name: str, *args, **kwargs):
+            if self.is_prod:
+                table_name += "-prod"
+            else:
+                table_name += "-dev"
 
+            return func(self, table_name, *args, **kwargs)
+
+        return wrapper
+
+    @add_env_suffix
     def put_data(self, table_name: str, data):
-        if self.is_prod:
-            table_name += "-prod"
-        else:
-            table_name += "-dev"
-
         table = self.resource.Table(table_name)
-
         table.put_item(Item=data)
 
+    @add_env_suffix
     def get_all(self, table_name: str):
-        if self.is_prod:
-            table_name += "-prod"
-        else:
-            table_name += "-dev"
-
         table = self.resource.Table(table_name)
-
         # Use the scan operation to retrieve all items from the table
         response = table.scan()
-
         # The response contains the items as well as other information like metadata
         items = response.get("Items", [])
 
