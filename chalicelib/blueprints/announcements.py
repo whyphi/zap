b85294a35abc79c7fa8a82c8336d33ab2f1c3612
Jin Young Bang <jybang@bu.edu>
2023-10-15 21:50:38 -0400

    chore: add TODOs for announcements route

--- chalicelib/blueprints/announcements.py
+++ chalicelib/blueprints/announcements.py
@@ -8,6 +8,14 @@ client = boto3.client('ses')
 
 @announcements_routes.route("/announcement")
 def send_announcement():
+    # TODO: 1. Get all email data from MongoDB (Consider creating a mongodb wrapper in services)
+    
+    # TODO: 2. Convert all emails to a list
+
+    # TODO: 3. Create a new object with the list initialized into to (either send To to techteampct@gmail.com or all the users)
+
+    # TODO: 4. Send Email using SesMailSender Class
+
     pass
 
 @announcements_routes.route("/test-email")
