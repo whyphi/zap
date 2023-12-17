44435c94f009fc3da183589e67a4282033ccc5aa
Jin Young Bang <jybang@bu.edu>
2023-12-17 11:43:46 -0500

    refactor: add service-based modules for listings

---
+++ chalicelib/services/ListingService.py
@@ -0,0 +1,21 @@
+class ListingService:
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+
+    def get(self):
+        pass
+
+    def get_all(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def toggle_visibility(self):
+        pass
+
+    def update_field_route(self):
+        pass
