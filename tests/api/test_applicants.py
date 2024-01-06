0dc7814b949171e5b66ea6040bdb7a33df8963f6
Jin Young Bang <jybang@bu.edu>
2024-01-06 16:58:30 +0900

    feat: create test suite for applicants API

---
+++ tests/api/test_applicants.py
@@ -0,0 +1,58 @@
+from chalice.app import Request
+from chalice.test import Client
+from unittest.mock import MagicMock, patch
+
+
+from app import app
+
+TEST_APPLICANTS = [
+    {"applicantId": "sample_id1", "name": "John Doe"},
+    {"applicantId": "sample_id2", "name": "Bob"},
+]
+
+
+def test_get_applicant():
+    # Create a Chalice test client
+    with Client(app) as client:
+        # Mock applicant_service's get method
+        with patch(
+            "chalicelib.services.ApplicantService.applicant_service.get"
+        ) as mock_get:
+            mock_get.return_value = TEST_APPLICANTS[0]
+            response = client.http.get(
+                f"/applicant/{TEST_APPLICANTS[0]['applicantId']}"
+            )
+
+            # Check the response status code and body
+            assert response.status_code == 200
+            assert response.json_body == TEST_APPLICANTS[0]
+
+
+def test_get_all_applicants():
+    # Create a Chalice test client
+    with Client(app) as client:
+        # Mock applicant_service's get method
+        with patch(
+            "chalicelib.services.ApplicantService.applicant_service.get_all"
+        ) as mock_get:
+            mock_get.return_value = TEST_APPLICANTS
+            response = client.http.get(f"/applicants")
+
+            # Check the response status code and body
+            assert response.status_code == 200
+            assert response.json_body == TEST_APPLICANTS
+
+
+def test_get_all_applicants_from_listing():
+    # Create a Chalice test client
+    with Client(app) as client:
+        # Mock applicant_service's get method
+        with patch(
+            "chalicelib.services.ApplicantService.applicant_service.get_all_from_listing"
+        ) as mock_get:
+            mock_get.return_value = TEST_APPLICANTS
+            response = client.http.get(f"/applicants/test_listing_id")
+
+            # Check the response status code and body
+            assert response.status_code == 200
+            assert response.json_body == TEST_APPLICANTS
