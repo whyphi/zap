ababd8fc780d57c09f676aac847f93bd68538905
Chris Gough <cwgough@bu.edu>
2024-01-06 13:50:45 -0800

    announcements route now accepts subject & content from frontend

--- chalicelib/blueprints/announcements.py
+++ chalicelib/blueprints/announcements.py
@@ -7,19 +7,21 @@ announcements_routes = Blueprint(__name__)
 
 ses_client = boto3.client('ses')
 
-message = """
-            Test email
-        """
 
-
-@announcements_routes.route("/announcement")
+# why is GET needed?
+@announcements_routes.route("/announcement", methods=["GET", "POST"], cors=True)
 def send_announcement():
     MongoServer = MongoService()
     MongoServer.connect()
     users_data = MongoServer.get_all_data()
 
-    # emails = [user['email'] for user in users_data]
-    emails = ['cwgough@bu.edu', 'cwilliam.gough@gmail.com']  # PLEASE REPLACE WITH YOUR OWN EMAIL FOR TESTING
+    emails = [user['email'] for user in users_data]
+
+    # PLEASE REPLACE WITH YOUR OWN EMAIL FOR TESTING
+    # emails = ['cwilliam.gough@gmail.com']
+
+    request = announcements_routes.current_request
+    request_body = request.json_body
 
     ses = SesMailSender(ses_client)
     for email in emails:
@@ -29,25 +31,25 @@ def send_announcement():
         ses.send_email(
             source="techteampct@gmail.com",
             destination=ses_destination,
-            subject="Hello World",
-            text="test",
-            html=message
+            subject=request_body['subject'],
+            text=request_body['content'],
+            html=request_body['content']
         )
 
     return f'Sent emails to {len(emails)} people: {emails}'
 
 
-@announcements_routes.route("/test-email")
-def test_email():
-    ses_destination = SesDestination(
-        tos=["techteampct@gmail.com"]
-    )
-    ses = SesMailSender(ses_client)
-    ses.send_email(
-        source="techteampct@gmail.com",
-        destination=ses_destination,
-        subject="Subject Test",
-        text="test data",
-        html="<h1>test</h1>"
-    )
-    return 'success'
+# @announcements_routes.route("/test-email")
+# def test_email():
+#     ses_destination = SesDestination(
+#         tos=["techteampct@gmail.com"]
+#     )
+#     ses = SesMailSender(ses_client)
+#     ses.send_email(
+#         source="techteampct@gmail.com",
+#         destination=ses_destination,
+#         subject="Subject Test",
+#         text="test data",
+#         html="<h1>test</h1>"
+#     )
+#     return 'success'
