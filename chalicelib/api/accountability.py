4e28b930dbdb109387ce20259c445613631b84d3
ojnim <seungmincho03@gmail.com>
2024-02-03 02:31:40 -0500

    fix

--- chalicelib/api/accountability.py
+++ chalicelib/api/accountability.py
@@ -1,7 +1,7 @@
 
 from chalicelib.db import DBResource
 from chalice import Blueprint
-import spreadsheetapi
+from chalicelib.api.spreadsheetapi import getdata, extractFileId
 
 
 import uuid
@@ -9,21 +9,22 @@ accountability_api = Blueprint(__name__)
 db = DBResource()
 
 #Manager Semesters
-@accountability_api.route("/semester",methods=["POST"],corse=True)
+@accountability_api.route("/semester",methods=["POST"],cors=True)
 def add_semester():
     """add new semester with given information"""
+    table_name = "zap-tracker"
     data = accountability_api.current_request.json_body
 
     listing_id = str(uuid.uuid4())
-    data["listingId"] = listing_id
+    data["semesterId"] = listing_id
     
-    db.put_data(table_name="semester-listing", data=data)
+    db.put_data(table_name, data)
     return {"msg":True}
 
-@accountability_api.route("/semester/list",methods=["GET"],corse=True)
+@accountability_api.route("/semester/list",methods=["GET"],cors=True)
 def semester_list():
     """get the list of semesters"""
-    data = db.get_all(table_name="semester-listing")
+    data = db.get_all(table_name="zap-tracker")
     return data
 
 #Manage Google Spreadsheet API and Data
@@ -33,9 +34,9 @@ def GoogleSheet():
         request_data = accountability_api.current_request.json_body
         url = request_data.get('link')
 
-        fileId = spreadsheetapi.extractFileId(url)
+        fileId = extractFileId(url)
         datarange = 'Brothers'
-        data = spreadsheetapi.getdata(fileId, datarange)
+        data = getdata(fileId, datarange)
 
         if data is None:
             return {'msg': False}
