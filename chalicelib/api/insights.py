0d746ca47c34151e96cfe196ad8a002899a9d9f3
Jin Young Bang <jybang@bu.edu>
2024-03-03 20:22:50 -0500

    feat: implement route protection through JWT

--- chalicelib/api/insights.py
+++ chalicelib/api/insights.py
@@ -2,12 +2,14 @@
 from chalice import Blueprint
 from chalicelib.services.InsightsService import insights_service
 from chalicelib.handlers.error_handler import handle_exceptions
+from chalicelib.decorators import auth
 from pydantic import ValidationError
 
 insights_api = Blueprint(__name__)
 
 
 @insights_api.route("/insights/listing/{listing_id}", methods=["GET"], cors=True)
+@auth(insights_api, role=["admin", "member"])
 def get_listing_insights(listing_id):
     """Get insights from <listing_id>"""
     return insights_service.get_insights_from_listing(listing_id)
--- chalicelib/decorators.py
+++ chalicelib/decorators.py
@@ -1,3 +1,7 @@
+import boto3
+import jwt
+
+
 def add_env_suffix(func):
     def wrapper(self, table_name: str, *args, **kwargs):
         if "env" in kwargs and kwargs["env"]:
@@ -8,3 +12,35 @@ def add_env_suffix(func):
         return func(self, table_name, *args, **kwargs)
 
     return wrapper
+
+
+def auth(blueprint, role):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            api_request = blueprint.current_request
+            auth_header = api_request.headers.get("Authorization", None)
+            _, token = auth_header.split(" ", 1) if " " in auth_header else (None, None)
+
+            if not token:
+                return {"error": "Authorization header is missing"}, 401
+
+            try:
+                ssm_client = boto3.client("ssm")
+                auth_secret = ssm_client.get_parameter(
+                    Name="/Zap/AUTH_SECRET", WithDecryption=True
+                )["Parameter"]["Value"]
+                decoded = jwt.decode(token, auth_secret, algorithms=["HS256"])
+                print(role)
+                # TODO: if decoded role is not part of given, reject auth
+
+                return func(*args, **kwargs)
+
+            except jwt.ExpiredSignatureError:
+                return {"error": "Unauthorized: Token has expired"}, 401
+            except jwt.InvalidTokenError:
+                print("test")
+                return {"error": "Unauthorized: Invalid token"}, 401
+
+        return wrapper
+
+    return decorator
