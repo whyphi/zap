f68bfe62991c581734561fbdf386a7907ae2ffd1
Jin Young Bang <jybang@bu.edu>
2024-03-26 09:20:00 -0400

    feat: implement member onboarding API

--- chalicelib/api/members.py
+++ chalicelib/api/members.py
@@ -8,5 +8,20 @@ members_api = Blueprint(__name__)
 @members_api.route("/members", methods=["GET"], cors=True)
 @auth(members_api, roles=["admin", "member"])
 def get_all_members():
-    """Get an applicant from <applicant_id>"""
+    """Fetches all members who have access to WhyPhi."""
     return member_service.get_all()
+
+
+@members_api.route("/members/onboard/{user_id}", methods=["POST"], cors=True)
+@auth(members_api, roles=[])
+def onboard_member(user_id):
+    data = members_api.current_request.json_body
+    data["isNewUser"] = False
+
+    if member_service.onboard(user_id, data):
+        return {
+            "status": True,
+            "message": "User updated successfully.",
+        }
+    else:
+        { "status": False}
