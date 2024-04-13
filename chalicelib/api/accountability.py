99a10851aeae4199f0fec30a28f7f3dcca4f35e4
Jin Young Bang <jybang@bu.edu>
2024-04-13 17:41:18 -0400

    feat: create accountability status API

--- chalicelib/api/accountability.py
+++ chalicelib/api/accountability.py
@@ -1,10 +1,18 @@
 from chalice import Blueprint
 from chalicelib.decorators import auth
+from chalicelib.services.AccountabilityService import accountability_service
 
 accountability_api = Blueprint(__name__)
 
 
-@accountability_api.route("/timeframes", methods=["POST"], cors=True)
-@auth(accountability_api, roles=["admin"])
+@accountability_api.route("/accountability", methods=["GET"], cors=True)
+# @auth(accountability_api, roles=["admin"])
 def get_accountability():
-    return accountability_service.get_accountability()
+    if accountability_api.current_request.query_params:
+        page = int(accountability_api.current_request.query_params.get("page", 0))
+        page_size = int(
+            accountability_api.current_request.query_params.get("page_size", 20)
+        )
+    else:
+        page, page_size = 0, 20
+    return accountability_service.get_total_accountability(page, page_size)
--- chalicelib/services/AccountabilityService.py
+++ chalicelib/services/AccountabilityService.py
@@ -4,10 +4,43 @@ from chalicelib.modules.google_sheets import GoogleSheetsModule
 
 class AccountabilityService:
     def __init__(self):
-        pass
+        # Currently hardcoded --> need to set as dynamic parameter either in DB or SSM
+        self.spreadsheet_id = "1VEj06zGrxq-1Hrp8ArbYJkvPw58g5e4C3MhDeBjL6o4"
+        self.gs = GoogleSheetsModule()
 
-    def get_accountability(self):
-        pass
+    def get_total_accountability(self, page: int, page_size: int = 20) -> dict[list]:
+        START_ROW = 2 + page * page_size
+        cells = self.gs.get_rows(
+            spreadsheet_id=self.spreadsheet_id,
+            sheet_name="Total",
+            start_row=START_ROW,
+            end_row=START_ROW + page_size - 1,
+        )
+        values = cells["values"]
+        values = [
+            {
+                "name": f"{value[0]} {value[1]}",
+                "status": value[2],
+                "currentPoints": int(value[3]),
+                "required": int(value[4]),
+                "chapterBucketMet": True if "YES" == value[9] else False,
+                "rushBucketMet": True if "YES" == value[10] else False,
+                "fundraisingBucketMet": True if "YES" == value[11] else False,
+                "eventsBucketMet": True if "YES" == value[12] else False,
+                "teamsBucketMet": True if "YES" == value[13] else False,
+                "variableBucketMet": True if "YES" == value[14] else False,
+            }
+            for value in values
+        ]
+        return values
+
+    def get_accountability_from_leaderboard(self) -> dict[list]:
+        cells = self.gs.get_all_cells(self.spreadsheet_id, "Leaderboard")
+        values = cells["values"][1:]
+        values = [
+            {"name": f"{value[1]} {value[2]}", "score": value[3]} for value in values
+        ]
+        return values
 
 
 accountability_service = AccountabilityService()
