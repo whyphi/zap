fa99f888e7a4f1e5c2deff8258dd4cb3e1ce8209
Jin Young Bang <jybang@bu.edu>
2024-06-29 18:50:46 -0400

    feat: integrate sentry into zap

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import os
+import boto3
+import sentry_sdk
+from sentry_sdk.integrations.chalice import ChaliceIntegration
+
 from chalice import Chalice
 
 from chalicelib.api.listings import listings_api
@@ -8,6 +13,22 @@ from chalicelib.api.members import members_api
 from chalicelib.api.events import events_api
 from chalicelib.api.accountability import accountability_api
 from chalicelib.api.monitoring import monitoring_api
+
+# Configure and initialize sentry
+sentry_sdk.init(
+    dsn=boto3.client("ssm").get_parameter(Name="/Zap/sentry/dsn", WithDecryption=True)[
+        "Parameter"
+    ]["Value"],
+    integrations=[ChaliceIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+    environment="production" if os.environ.get("ENV") == "prod" else "development",
+)
 
 app = Chalice(app_name="zap")
 app.register_blueprint(announcements_api)
@@ -22,4 +43,5 @@ app.register_blueprint(monitoring_api)
 
 @app.route("/")
 def index():
+    1 / 0  # raises an error
     return {"hello": "world"}
