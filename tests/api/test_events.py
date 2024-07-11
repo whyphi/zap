6ac88d676a55e45d869caaa892e8f46e58b21910
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-07-10 22:32:05 -0400

    added remaining API tests for events_service

--- tests/api/test_events.py
+++ tests/api/test_events.py
@@ -10,6 +10,12 @@ with open("tests/fixtures/events/general/sample_timeframes.json") as f:
 with open("tests/fixtures/events/general/sample_timeframe_sheets.json") as f:
     SAMPLE_TIMEFRAME_SHEETS = json.load(f)
 
+with open("tests/fixtures/events/general/sample_rush_events.json") as f:
+    SAMPLE_RUSH_EVENTS = json.load(f)
+
+with open("tests/fixtures/events/general/sample_rush_event.json") as f:
+    SAMPLE_RUSH_EVENT = json.load(f)
+
 def test_create_timeframe():
     with Client(app) as client:
         with patch("chalicelib.decorators.jwt.decode") as mock_decode:
@@ -68,23 +74,22 @@ def test_get_timeframe():
                 assert response.status_code == 200
                 assert response.json_body == SAMPLE_TIMEFRAMES[0]
 
-# TODO: fix bug with delete_timeframe (getting `500 internal service error`)
-# def test_delete_timeframe():
-#     with Client(app) as client:
-#         with patch("chalicelib.decorators.jwt.decode") as mock_decode:
-#             # Assuming the decoded token has the required role
-#             mock_decode.return_value = {"role": "admin"}
-#             with patch(
-#                 "chalicelib.services.EventService.event_service.get_timeframe",
-#             ) as mock_delete:
-#                 mock_delete.return_value = {"status": True}
-#                 response = client.http.delete(
-#                     f"/timeframes/{SAMPLE_TIMEFRAMES[0]['_id']}",
-#                     headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
-#                 )
+def test_delete_timeframe():
+    with Client(app) as client:
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.delete_timeframe",
+            ) as mock_delete:
+                mock_delete.return_value = {"status": True}
+                response = client.http.delete(
+                    f"/timeframes/{SAMPLE_TIMEFRAMES[0]['_id']}",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
-#                 assert response.status_code == 200
-#                 # assert response.json_body == None
+                assert response.status_code == 200
+                assert response.json_body == {'status': True}
 
 
 def test_create_event():
@@ -144,160 +149,178 @@ def test_get_timeframe_sheets():
                 assert response.status_code == 200
                 assert response.json_body == SAMPLE_TIMEFRAME_SHEETS
 
-# def get_timeframe_sheets(timeframe_id: str):
+def test_checkin():
+    with Client(app) as client:
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.checkin"
+            ) as mock_checkin:
+                mock_checkin.return_value = {"msg": True}
+                response = client.http.post(
+                    f"/events/test_event_id/checkin",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
-# def checkin(event_id: str):
+                assert response.status_code == 200
+                assert response.json_body == {"msg": True}
 
-# def delete_event(event_id: str):
+def test_delete_event():
+    with Client(app) as client:
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.delete",
+            ) as mock_delete:
+                mock_delete.return_value = {"status": True}
+                response = client.http.delete(
+                    f"/events/test_event_id",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
-# def get_rush_events():
+                assert response.status_code == 200
+                assert response.json_body == {'status': True}
 
-# def get_rush_event(event_id):
+def test_get_rush_events():
+    # Create a Chalice test client
+    with Client(app) as client:
+        # Mock applicant_service's get method
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.get_rush_categories_and_events",
+            ) as mock_get_rush_categories_and_events:
+                mock_get_rush_categories_and_events.return_value = SAMPLE_RUSH_EVENTS
+                response = client.http.get(
+                    f"/events/rush",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
-# def create_rush_category():
+                # Check the response status code and body
+                assert response.status_code == 200
+                assert response.json_body == SAMPLE_RUSH_EVENTS
 
-# def create_rush_event():
+def test_get_rush_event():
+    # Create a Chalice test client
+    with Client(app) as client:
+        # Mock applicant_service's get method
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.get_rush_event",
+            ) as mock_get_rush_event:
+                mock_get_rush_event.return_value = SAMPLE_RUSH_EVENT
+                response = client.http.get(
+                    f"/events/rush/test_event_id",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
-# def modify_rush_event():
+                # Check the response status code and body
+                assert response.status_code == 200
+                assert response.json_body == SAMPLE_RUSH_EVENT
 
-# def modify_rush_settings():
+def test_create_rush_category():
+    with Client(app) as client:
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.create_rush_category"
+            ) as mock_create_rush_category:
+                mock_create_rush_category.return_value = {"msg": True}
+                response = client.http.post(
+                    f"/events/rush/category",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
-# def checkin_rush(event_id):
+                assert response.status_code == 200
+                assert response.json_body == {"msg": True}
 
-# def delete_rush_event(event_id):
+def test_create_rush_event():
+    with Client(app) as client:
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.create_rush_event"
+            ) as mock_create_rush_event:
+                mock_create_rush_event.return_value = {"msg": True}
+                response = client.http.post(
+                    f"/events/rush",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
+                assert response.status_code == 200
+                assert response.json_body == {"msg": True}
 
-# def test_get_all_listings():
-#     with Client(app) as client:
-#         with patch("chalicelib.decorators.jwt.decode") as mock_decode:
-#             # Assuming the decoded token has the required role
-#             mock_decode.return_value = {"role": "admin"}
-#             with patch(
-#                 "chalicelib.services.ListingService.listing_service.get_all"
-#             ) as mock_get_all:
-#                 mock_get_all.return_value = TEST_LISTINGS
-#                 response = client.http.get(
-#                     f"/listings",
-#                     headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
-#                 )
+def test_modify_rush_event():
+    with Client(app) as client:
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.modify_rush_event"
+            ) as mock_modify_rush_event:
+                mock_modify_rush_event.return_value = {"msg": True}
+                response = client.http.patch(
+                    f"/events/rush",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
-#                 assert response.status_code == 200
-#                 assert response.json_body == TEST_LISTINGS
+                assert response.status_code == 200
+                assert response.json_body == {"msg": True}
 
+def test_modify_rush_settings():
+    with Client(app) as client:
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.modify_rush_settings"
+            ) as mock_modify_rush_settings:
+                mock_modify_rush_settings.return_value = {"msg": True}
+                response = client.http.patch(
+                    f"/events/rush/settings",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
-# def test_delete_listing():
-#     with Client(app) as client:
-#         with patch("chalicelib.decorators.jwt.decode") as mock_decode:
-#             # Assuming the decoded token has the required role
-#             mock_decode.return_value = {"role": "admin"}
-#             with patch(
-#                 "chalicelib.services.ListingService.listing_service.delete"
-#             ) as mock_delete:
-#                 mock_delete.return_value = {"status": True}
-#                 response = client.http.delete(
-#                     f"/listings/{TEST_LISTINGS[0]['listingId']}",
-#                     headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
-#                 )
+                assert response.status_code == 200
+                assert response.json_body == {"msg": True}
 
-#                 assert response.status_code == 200
-#                 assert response.json_body == {"status": True}
+def test_checkin_rush():
+    with Client(app) as client:
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.checkin_rush"
+            ) as mock_checkin_rush:
+                mock_checkin_rush.return_value = {"msg": True}
+                response = client.http.post(
+                    f"/events/rush/checkin/test_event_id",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
+                assert response.status_code == 200
+                assert response.json_body == {"msg": True}
 
-# def test_toggle_visibility():
-#     with Client(app) as client:
-#         with patch("chalicelib.decorators.jwt.decode") as mock_decode:
-#             # Assuming the decoded token has the required role
-#             mock_decode.return_value = {"role": "admin"}
-#             with patch(
-#                 "chalicelib.services.ListingService.listing_service.toggle_visibility"
-#             ) as mock_toggle_visibility:
-#                 mock_toggle_visibility.return_value = {"status": True}
-#                 response = client.http.patch(
-#                     f"/listings/{TEST_LISTINGS[0]['listingId']}/toggle/visibility",
-#                     headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
-#                 )
+def test_delete_rush_event():
+    with Client(app) as client:
+        with patch("chalicelib.decorators.jwt.decode") as mock_decode:
+            # Assuming the decoded token has the required role
+            mock_decode.return_value = {"role": "admin"}
+            with patch(
+                "chalicelib.services.EventService.event_service.delete_rush_event",
+            ) as mock_delete_rush_event:
+                mock_delete_rush_event.return_value = {"status": True}
+                response = client.http.delete(
+                    f"/events/rush/test_event_id",
+                    headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
+                )
 
-#                 assert response.status_code == 200
-#                 assert response.json_body == {"status": True}
-
-
-# def test_update_listing_field_route():
-#     with Client(app) as client:
-#         with patch("chalicelib.decorators.jwt.decode") as mock_decode:
-#             # Assuming the decoded token has the required role
-#             mock_decode.return_value = {"role": "admin"}
-#             with patch(
-#                 "chalicelib.services.ListingService.listing_service.update_field_route"
-#             ) as mock_update_field_route:
-#                 mock_update_field_route.return_value = {
-#                     "status": True,
-#                     "updated_listing": TEST_LISTINGS[1],
-#                 }
-#                 response = client.http.patch(
-#                     f"/listings/{TEST_LISTINGS[1]['listingId']}/update-field",
-#                     headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
-#                 )
-
-#                 print(response.json_body)
-
-#                 assert response.status_code == 200
-#                 assert response.json_body == {
-#                     "status": True,
-#                     "updated_listing": TEST_LISTINGS[1],
-#                 }
-
-
-# def test_update_listing_field_route_not_found():
-#     with Client(app) as client:
-#         with patch("chalicelib.decorators.jwt.decode") as mock_decode:
-#             # Assuming the decoded token has the required role
-#             mock_decode.return_value = {"role": "admin"}
-#             with patch(
-#                 "chalicelib.services.ListingService.listing_service.update_field_route"
-#             ) as mock_update_field_route:
-#                 mock_update_field_route.side_effect = NotFoundError("Not found")
-
-#                 response = client.http.patch(
-#                     f"/listings/{TEST_LISTINGS[1]['listingId']}/update-field",
-#                     headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
-#                 )
-
-#                 assert response.json_body == None
-
-
-# def test_update_listing_field_route_bad_request():
-#     with Client(app) as client:
-#         with patch("chalicelib.decorators.jwt.decode") as mock_decode:
-#             # Assuming the decoded token has the required role
-#             mock_decode.return_value = {"role": "admin"}
-#             with patch(
-#                 "chalicelib.services.ListingService.listing_service.update_field_route"
-#             ) as mock_update_field_route:
-#                 mock_update_field_route.side_effect = BadRequestError("Bad request")
-
-#                 response = client.http.patch(
-#                     f"/listings/{TEST_LISTINGS[1]['listingId']}/update-field",
-#                     headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
-#                 )
-
-#                 # body, status_code = response.json_body
-
-#                 assert response.json_body == None
-
-
-# def test_update_listing_field_route_exception():
-#     with Client(app) as client:
-#         with patch("chalicelib.decorators.jwt.decode") as mock_decode:
-#             # Assuming the decoded token has the required role
-#             mock_decode.return_value = {"role": "admin"}
-#             with patch(
-#                 "chalicelib.services.ListingService.listing_service.update_field_route"
-#             ) as mock_update_field_route:
-#                 mock_update_field_route.side_effect = Exception("Error")
-
-#                 response = client.http.patch(
-#                     f"/listings/{TEST_LISTINGS[1]['listingId']}/update-field",
-#                     headers={"Authorization": "Bearer SAMPLE_TOKEN_STRING"},
-#                 )
-
-#                 assert response.json_body == None
+                assert response.status_code == 200
+                assert response.json_body == {'status': True}
