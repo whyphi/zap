f88d74b20d0cf3dfc9f51c5d69cbb93d146d2b04
MattFromBeijing <mhyan@bu.edu>
2025-03-17 17:52:39 -0400

    feat: now scrapes 3 other job boards

--- chalicelib/services/JobPostingService.py
+++ chalicelib/services/JobPostingService.py
@@ -14,12 +14,10 @@ class JobPostingService:
         options.add_argument("--disable-gpu")  # Necessary for some environments
         options.add_argument("--no-sandbox")  # Good practice for running in Docker/Linux
         webDriver = webdriver.Chrome(service=service, options=options)
-        
         self.driver = webDriver
     
-    def getTechJobs(self) -> List:
-        page_url = "https://github.com/SimplifyJobs/Summer2025-Internships/blob/dev/README.md"
-        self.driver.get(page_url)
+    def getJobs(self, urlStr) -> List:
+        self.driver.get(urlStr)
     
         table = self.driver.find_element(By.TAG_NAME, "table")
         body = table.find_element(By.TAG_NAME, "tbody")
@@ -33,19 +31,29 @@ class JobPostingService:
             if (self.isMoreThanOneWeekAgo(date)):
                 break
             
+            company = ""
+            role = ""
+            link = ""
+            
             try:
                 company = cols[0].find_element(By.TAG_NAME, "strong").find_element(By.TAG_NAME, "a").text
                 prevRowCompany = company
             except NoSuchElementException:
                 company = prevRowCompany
             
-            role = cols[1].text
-            link = cols[3].find_element(By.TAG_NAME, "a").get_attribute("href")
+            try:
+                role = cols[1].find_element(By.TAG_NAME, "strong").find_element(By.TAG_NAME, "a").text
+                link = cols[1].find_element(By.TAG_NAME, "strong").find_element(By.TAG_NAME, "a").get_attribute("href")
+            except:
+                role = cols[1].text
             
-            newJob = [company, 
-                      role,
-                      link,
-                      date]
+            if link == "":
+                link = cols[3].find_element(By.TAG_NAME, "a").get_attribute("href")
+                
+            newJob = {"company": company, 
+                      "role": role,
+                      "link": link,
+                      "date": date}
             jobs.append(newJob)
         
         return jobs
@@ -60,3 +68,14 @@ class JobPostingService:
         return False
     
 job_posting_service = JobPostingService()
+
+if __name__ == "__main__":
+    test_service = JobPostingService()
+    links = [('Simplify (Tech)', 'https://github.com/SimplifyJobs/Summer2025-Internships/blob/dev/README.md'), 
+             ('Cvrve (Tech)','https://github.com/cvrve/Summer2025-Internships/blob/dev/README.md'), 
+             ('Jobright.ai (Consulting)','https://github.com/jobright-ai/2025-Consultant-Internship/blob/master/README.md'), 
+             ('Jobright.ai (Marketing)','https://github.com/jobright-ai/2025-Marketing-Internship/blob/master/README.md')]
+    
+    for source, link in links:
+        postings = test_service.getJobs(link)
+        print(f"{source}:\n{postings}\n\n\n")
