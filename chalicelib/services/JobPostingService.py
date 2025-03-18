75b6747d843482beb785ecd713bc576dcf6b34db
Vincent Li <vincentli0921@gmail.com>
2025-03-18 14:11:13 -0400

    updated get jobs function to scrape from multiple repositories

--- chalicelib/services/JobPostingService.py
+++ chalicelib/services/JobPostingService.py
@@ -16,13 +16,11 @@ class JobPostingService:
         options.add_argument("--disable-gpu")  # Necessary for some environments
         options.add_argument("--no-sandbox")  # Good practice for running in Docker/Linux
         webDriver = webdriver.Chrome(service=service, options=options)
-        
         self.driver = webDriver
         self.gs = GoogleSheetsModule()
     
-    def getTechJobs(self) -> List[Dict]:
-        page_url = "https://github.com/SimplifyJobs/Summer2025-Internships/blob/dev/README.md"
-        self.driver.get(page_url)
+    def getJobs(self, urlStr) -> List:
+        self.driver.get(urlStr)
     
         table = self.driver.find_element(By.TAG_NAME, "table")
         body = table.find_element(By.TAG_NAME, "tbody")
@@ -36,19 +34,29 @@ class JobPostingService:
             if (self.isMoreThanOneWeekAgo(date)):
                 break
             
+            company = ""
+            role = ""
+            link = ""
+            
             try:
                 company = cols[0].find_element(By.TAG_NAME, "strong").find_element(By.TAG_NAME, "a").text
                 prevRowCompany = company
             except NoSuchElementException:
                 company = prevRowCompany
             
-            role = cols[1].text
-            link = cols[3].find_element(By.TAG_NAME, "a").get_attribute("href")
+            try:
+                role = cols[1].find_element(By.TAG_NAME, "strong").find_element(By.TAG_NAME, "a").text
+                link = cols[1].find_element(By.TAG_NAME, "strong").find_element(By.TAG_NAME, "a").get_attribute("href")
+            except NoSuchElementException:
+                role = cols[1].text
             
-            newJob = [company, 
-                      role,
-                      link,
-                      date]
+            if link == "":
+                link = cols[3].find_element(By.TAG_NAME, "a").get_attribute("href")
+                
+            newJob = {"company": company, 
+                      "role": role,
+                      "link": link,
+                      "date": date}
             jobs.append(newJob)
         
         return jobs
