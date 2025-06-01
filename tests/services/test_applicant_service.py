9bc8f9556b2ced5677b09d6abce952646d9ef01b
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-06-01 13:18:56 -0400

    refactored test 1 (applications)

--- tests/services/test_applicant_service.py
+++ tests/services/test_applicant_service.py
@@ -1,59 +1,68 @@
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from chalicelib.services.ApplicantService import ApplicantService
+from chalicelib.services.ListingService import ListingService
 
 
 @pytest.fixture
 def service():
-    with patch("chalicelib.services.ApplicantService.db") as mock_db:
-        yield ApplicantService(), mock_db
+    with patch(
+        "chalicelib.services.ApplicantService.RepositoryFactory"
+    ) as mock_factory:
+        mock_applicants_repo = Mock()
+        mock_listings_repo = Mock()
+        mock_factory.applications.return_value = mock_applicants_repo
+        mock_factory.listings.return_value = mock_listings_repo
+
+        # services: will use the patched RepositoryFactory
+        applicants_service = ApplicantService()
+        listings_service = ListingService()
+        yield applicants_service, listings_service, mock_applicants_repo, mock_listings_repo
 
 
 def test_get_applicant(service):
-    applicant_service, mock_db = service
+    applicants_service, _, mock_applicants_repo, _ = service
 
-    sample_data = {"applicantId": "sample_id", "name": "John Doe"}
-    mock_db.get_item.return_value = sample_data
+    sample_data = {"id": "sample_id", "name": "John Doe"}
+    mock_applicants_repo.get_by_id.return_value = sample_data
 
-    result = applicant_service.get("sample_id")
-    mock_db.get_item.assert_called_once_with(
-        table_name="zap-applications", key={"applicantId": "sample_id"}
-    )
+    result = applicants_service.get("sample_id")
+    mock_applicants_repo.get_by_id.assert_called_once_with(id_value="sample_id")
 
     assert result == sample_data
 
 
-def test_get_all_applicants(service):
-    applicant_service, mock_db = service
+# def test_get_all_applicants(service):
+#     applicant_service, mock_db = service
 
-    sample_data = [
-        {"applicantId": "sample_id1", "name": "John Doe"},
-        {"applicantId": "sample_id2", "name": "Bob"},
-    ]
-    mock_db.get_all.return_value = sample_data
+#     sample_data = [
+#         {"applicantId": "sample_id1", "name": "John Doe"},
+#         {"applicantId": "sample_id2", "name": "Bob"},
+#     ]
+#     mock_db.get_all.return_value = sample_data
 
-    result = applicant_service.get_all()
-    mock_db.get_all.assert_called_once_with(table_name="zap-applications")
+#     result = applicant_service.get_all()
+#     mock_db.get_all.assert_called_once_with(table_name="zap-applications")
 
-    assert result == sample_data
-    assert len(result) == 2
+#     assert result == sample_data
+#     assert len(result) == 2
 
 
-def test_get_all_applicants_from_listing(service):
-    applicant_service, mock_db = service
+# def test_get_all_applicants_from_listing(service):
+#     applicant_service, mock_db = service
 
-    listing_id = "1"
-    sample_data = [
-        {"applicantId": "sample_id1", "name": "John Doe"},
-        {"applicantId": "sample_id2", "name": "Bob"},
-    ]
-    mock_db.get_applicants.return_value = sample_data
-    mock_db.get_item.return_value = {}
+#     listing_id = "1"
+#     sample_data = [
+#         {"applicantId": "sample_id1", "name": "John Doe"},
+#         {"applicantId": "sample_id2", "name": "Bob"},
+#     ]
+#     mock_db.get_applicants.return_value = sample_data
+#     mock_db.get_item.return_value = {}
 
-    result = applicant_service.get_all_from_listing(listing_id)
-    mock_db.get_applicants.assert_called_once_with(
-        table_name="zap-applications", listing_id=listing_id
-    )
+#     result = applicant_service.get_all_from_listing(listing_id)
+#     mock_db.get_applicants.assert_called_once_with(
+#         table_name="zap-applications", listing_id=listing_id
+#     )
 
-    assert result == sample_data
-    assert len(result) == 2
+#     assert result == sample_data
+#     assert len(result) == 2
