f9433860e7b8c9600d6ff7e956bdb8e16d09dadf
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-06-01 13:33:25 -0400

    refactored test 3 (applications)

--- tests/services/test_applicant_service.py
+++ tests/services/test_applicant_service.py
@@ -4,26 +4,27 @@ from chalicelib.services.ApplicantService import ApplicantService
 from chalicelib.services.ListingService import ListingService
 
 
-SAMPLE_LISTINGS = (
+SAMPLE_LISTING = {
+    "id": "1",
+    "title": "PCT Fall 2023 Rush Application",
+    "date_created": "2023-09-15T17:03:29.156Z",
+    "deadline": "2023-09-19T04:00:00.000Z",
+    "is_visible": True,
+    "is_encrypted": False,
+    "questions": [
         {
-            "id": "1",
-            "title": "PCT Fall 2023 Rush Application",
-            "date_created": "2023-09-15T17:03:29.156Z",
-            "deadline": "2023-09-19T04:00:00.000Z",
-            "is_visible": True,
-            "is_encrypted": False,
-            "questions": [
-                {
-                    "question": "Tell us about yourself. What are you passionate about/what motivates you? (200 words max)",
-                    "context": "",
-                },
-            ],
+            "question": "Tell us about yourself. What are you passionate about/what motivates you? (200 words max)",
+            "context": "",
         },
-    )
+    ],
+}
+
+
 SAMPLE_APPLICANTS = [
     {"id": "sample_id1", "name": "John Doe"},
     {"id": "sample_id2", "name": "Bob"},
 ]
+
 
 @pytest.fixture
 def service():
@@ -37,12 +38,11 @@ def service():
 
         # services: will use the patched RepositoryFactory
         applicants_service = ApplicantService()
-        listings_service = ListingService()
-        yield applicants_service, listings_service, mock_applicants_repo, mock_listings_repo
+        yield applicants_service, mock_applicants_repo, mock_listings_repo
 
 
 def test_get_applicant(service):
-    applicants_service, _, mock_applicants_repo, _ = service
+    applicants_service, mock_applicants_repo, _ = service
 
     mock_applicants_repo.get_by_id.return_value = SAMPLE_APPLICANTS
 
@@ -53,7 +53,7 @@ def test_get_applicant(service):
 
 
 def test_get_all_applicants(service):
-    applicants_service, _, mock_applicants_repo, _ = service
+    applicants_service, mock_applicants_repo, _ = service
 
     mock_applicants_repo.get_all.return_value = SAMPLE_APPLICANTS
 
@@ -64,18 +64,18 @@ def test_get_all_applicants(service):
     assert len(result) == 2
 
 
-# def test_get_all_applicants_from_listing_unencrypted(service):
-#     applicants_service, _, mock_applicants_repo, _ = service
+def test_get_all_applicants_from_listing_unencrypted(service):
+    applicants_service, mock_applicants_repo, mock_listings_repo = service
 
-#     listing_id = "1"
-    
-#     mock_db.get_applicants.return_value = SAMPLE_APPLICANTS
-#     mock_db.get_item.return_value = {}
+    mock_listings_repo.get_by_id.return_value = SAMPLE_LISTING
+    mock_applicants_repo.get_all_by_field.return_value = SAMPLE_APPLICANTS
 
-#     result = applicant_service.get_all_from_listing(listing_id)
-#     mock_db.get_applicants.assert_called_once_with(
-#         table_name="zap-applications", listing_id=listing_id
-#     )
+    result = applicants_service.get_all_from_listing(SAMPLE_LISTING["id"])
 
-#     assert result == SAMPLE_APPLICANTS
-#     assert len(result) == 2
+    mock_listings_repo.get_by_id.assert_called_once_with(id_value=SAMPLE_LISTING["id"])
+    mock_applicants_repo.get_all_by_field.assert_called_once_with(
+        field="listing_id", value=SAMPLE_LISTING["id"]
+    )
+
+    assert result == SAMPLE_APPLICANTS
+    assert len(result) == 2
