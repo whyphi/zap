0e4b1a0e655cf0ccc142fb09ff859d2533aa9b13
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-06-06 19:47:34 -0400

    added handle exceptions

--- chalicelib/api/applicants.py
+++ chalicelib/api/applicants.py
@@ -10,6 +10,7 @@ applicants_api = Blueprint(__name__)
 
 @applicants_api.route("/applicant/{id}", methods=["GET"], cors=True)
 @auth(applicants_api, roles=[Roles.ADMIN, Roles.MEMBER])
+@handle_exceptions
 def get_applicant(id):
     """Get an applicant from <applicant_id>"""
     return applicant_service.get(id)
@@ -17,12 +18,14 @@ def get_applicant(id):
 
 @applicants_api.route("/applicants", methods=["GET"], cors=True)
 @auth(applicants_api, roles=[Roles.ADMIN, Roles.MEMBER])
+@handle_exceptions
 def get_applicants():
     return applicant_service.get_all()
 
 
 @applicants_api.route("/applicants/{listing_id}", methods=["GET"], cors=True)
 @auth(applicants_api, roles=[Roles.ADMIN, Roles.MEMBER])
+@handle_exceptions
 def get_all_applicants(listing_id):
     """Gets all applicants from <listing_id>"""
     return applicant_service.get_all_from_listing(listing_id)
