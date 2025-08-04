e7eeb40fef76ae975d4408db42f313319411e6ef
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-08-04 19:24:32 -0400

    singleton client pattern (to avoid bottlenecks)

--- chalicelib/modules/supabase_client.py
+++ chalicelib/modules/supabase_client.py
@@ -2,31 +2,50 @@ import os
 from supabase import create_client, Client
 from chalicelib.modules.aws_ssm import aws_ssm
 from dotenv import load_dotenv
+from typing import Optional, Tuple
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class SupabaseClient:
-    def __init__(self):
+    _client: Optional[Client] = None
+
+    @staticmethod
+    def get_params() -> Tuple[str, str]:
         ENV = os.getenv("ENV", "local")
         # TODO: remove eventually (for debugging)
-        print(f"[supabase_client] Running in env: {ENV}")
+        logger.info(f"[SupabaseClient] Running in env: {ENV}")
 
         if ENV in ["staging", "prod"]:
-            self.url = aws_ssm.get_parameter_value(f"/Zap/{ENV}/SUPABASE_URL")
-            self.key = aws_ssm.get_parameter_value(f"/Zap/{ENV}/SUPABASE_KEY")
+            try:
+                url = aws_ssm.get_parameter_value(f"/Zap/{ENV}/SUPABASE_URL")
+                key = aws_ssm.get_parameter_value(f"/Zap/{ENV}/SUPABASE_KEY")
+            except Exception as e:
+                raise Exception(
+                    f"[SupabaseClient] Failed to fetch Supabase params from SSM: {str(e)}"
+                )
         elif ENV == "pytest-init":
             # Edge case: pytest lazy-initializes url and key
-            return
+            url = "dummy-url"
+            key = "dummy-key"
         else:
             load_dotenv(".env.local")
-            self.url = os.getenv("SUPABASE_URL")
-            self.key = os.getenv("SUPABASE_KEY")
+            url = os.getenv("SUPABASE_URL")
+            key = os.getenv("SUPABASE_KEY")
 
-        if not (self.url and self.key):
+        if not (url and key):
             raise Exception(
                 f"[SupabaseClient.__init__] Could not find Supabase url or key. ENV: {ENV}"
             )
 
-        self.client: Client = create_client(self.url, self.key)
+        return url, key
 
-    def get_client(self):
-        return self.client
+    @classmethod
+    def get_client(cls) -> Client:
+
+        if cls._client is None:
+            url, key = cls.get_params()
+            cls._client = create_client(url, key)
+
+        return cls._client
