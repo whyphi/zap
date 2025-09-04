eb848831e7a7b2ba0a7fb85b6258ca660bc4bf1d
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-09-03 20:30:41 -0400

    added note

--- chalicelib/utils/rush_events.py
+++ chalicelib/utils/rush_events.py
@@ -1,3 +1,4 @@
+# TODO: eventually this should be configurable in by admins for each rush-timeframe (to prevent "false-negative" thresholds)
 mandatory_events = ["Info Session 1", "Info Session 2"]
 remaining_events = ["Professional Panel", "Resume Night", "Social Event"]
 minimum_remaining_events = 2
@@ -15,7 +16,9 @@ def is_rush_threshold_met(events_attended: list[dict], events: dict) -> bool:
         bool: True if attended at least 1 mandatory and 2 remaining events
     """
     # Get names of attended events
-    attended_names = [events[ea["id"]]["name"] for ea in events_attended if ea.get("attended")]
+    attended_names = [
+        events[ea["id"]]["name"] for ea in events_attended if ea.get("attended")
+    ]
 
     has_attended_mandatory = any(name in mandatory_events for name in attended_names)
     attended_remaining = sum(1 for name in attended_names if name in remaining_events)
